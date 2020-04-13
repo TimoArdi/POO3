07aefd1eae4410fb0bfcf4a67e206d4caf1f5be6
TimoArdi <61334551+TimoArdi@users.noreply.github.com>
2020-04-13 18:20:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 // index.php
 require_once 'Bicycle.php';
 require_once 'Car.php';
-require_once 'Truck.php';
 require_once 'HighWay.php';
 require_once 'MotorWay.php';
 require_once 'PedestrianWay.php';
@@ -12,12 +11,7 @@ $bike = new Bicycle('bleue',15);
 $mustang = new Car('noire',2,'fuel');
 $bmw = new Car('blanche',5,'electric');
 $bicycle = new Bicycle('blue', 1);
-$monster = new Truck('red',8,'fuel',500);
-echo $monster->is_full();
-$monster->setchargement(500);
-echo $monster->is_full();
-echo $monster->forward();
-echo $monster->brake();
+
 $road66 = new MotorWay();
 $road66->addVehicle($bmw);
 $road66->addVehicle($bicycle);
